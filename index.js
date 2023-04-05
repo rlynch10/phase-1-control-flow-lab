0ecfa3c4d4bfc815da1da643fe500f502b0d112f
Robert Lynch <relynch10@gmail.com>
2023-04-05 00:49:25 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400)  {
+    ride = true
+    return "This one is on me!"
+  } else if (ride > 400 && ride <=1999) {
+    return "That will be twenty bucks."
+  } else if (ride > 2000 && ride <=2500){
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
+
+  }
+
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:
+      return "Bye."
+  }
 }
